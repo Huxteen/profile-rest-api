9293ca7b828421709e8f8daccfe184b1b2e5ae55
Augustine Okeke <hursteen@gmail.com>
2020-01-07 20:20:03 +0100

    setup custom users model database

---
+++ app/accounts/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+# Create your models here.
+
+
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with our custom user model. """
+
+    def create_user(self, email, name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address.')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password, **extra_fields):
+        """Create and save a new super user."""
+
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """User profiles we want to collect. """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        '''Returns the full name, with a space in between.'''
+        return self.name.strip()
+
+    def get_short_name(self):
+        '''Returns the short name for the user.'''
+        return self.name.strip()
+
+    def __str__(self):
+        """ Django uses this when it need to convert the object to a string """
+        return self.email
+
+
+
