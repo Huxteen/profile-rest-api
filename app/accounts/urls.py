45c04ffa1c85cdcac72722ee2dde6a4bc4aff5f4
Augustine Okeke <hursteen@gmail.com>
2020-01-08 20:59:09 +0100

    Added our Router and viewset

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from accounts import views
 
+# We use this when we use a viewset
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
 urlpatterns = [
-    path('', views.profile, name='profile'),
+    path('profile-home', views.profile, name='profile'),
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
 
 ]
    
