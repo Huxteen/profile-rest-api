18351cb26247a13bb1ddbc6fcae53ccffd5e2182
Augustine Okeke <hursteen@gmail.com>
2020-01-22 19:04:45 +0100

    Added the the model for profile feed items

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from accounts.models import UserProfile
+from accounts.models import UserProfile, ProfileFeedItem
 
 admin.site.register(UserProfile)
+admin.site.register(ProfileFeedItem)
